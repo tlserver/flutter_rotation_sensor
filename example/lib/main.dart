7ba24d4f0e659c42dbf33a7a7ce6c759411ba13b
6y <tlserver6y@gmail.com>
2024-04-07 18:53:30 +0800

    fixup! feat: add plugin functions

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,40 +10,38 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Rotation Sensor Example'),
-        ),
-        body: Center(
-          child: StreamBuilder(
-            stream:
-                RotationSensor.getOrientationStream(SensorInterval.uiInterval),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                final data = snapshot.data!;
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text('Euler:\n${formatEulerAngles(data.eulerAngles)}'),
-                    Text('Quaternion:\n${formatQuaternion(data.quaternion)}'),
-                    Text('Matrix:\n${formatMatrix(data.rotationMatrix)}'),
-                    Text('Accuracy:\n${formatDouble(data.accuracy)}'),
-                    Text('Timestamp:\n${data.timestamp}'),
-                  ],
-                );
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else {
-                return const CircularProgressIndicator();
-              }
-            },
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Rotation Sensor Example'),
+          ),
+          body: Center(
+            child: StreamBuilder(
+              stream: RotationSensor.getOrientationStream(
+                  SensorInterval.uiInterval),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final data = snapshot.data!;
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text('Euler:\n${formatEulerAngles(data.eulerAngles)}'),
+                      Text('Quaternion:\n${formatQuaternion(data.quaternion)}'),
+                      Text('Matrix:\n${formatMatrix(data.rotationMatrix)}'),
+                      Text('Accuracy:\n${formatDouble(data.accuracy)}'),
+                      Text('Timestamp:\n${data.timestamp}'),
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              },
+            ),
           ),
         ),
-      ),
-    );
-  }
+      );
 
   String formatQuaternion(Quaternion q) {
     final f = formatDouble;
@@ -63,7 +61,5 @@ class MyApp extends StatelessWidget {
     return '(${f(e.azimuth)}, ${f(e.pitch)}, ${f(e.roll)})';
   }
 
-  String formatDouble(double d) {
-    return d.toStringAsFixed(2).padLeft(5);
-  }
+  String formatDouble(double d) => d.toStringAsFixed(2).padLeft(5);
 }
