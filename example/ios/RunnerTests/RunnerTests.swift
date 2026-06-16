a7cc03ffa80a3df5fd904d2373f6015b212af816
6y <tlserver6y@gmail.com>
2026-06-16 23:53:03 +0800

    chore: recreate project by flutter 3.29.2

--- example/ios/RunnerTests/RunnerTests.swift
+++ example/ios/RunnerTests/RunnerTests.swift
@@ -2,6 +2,7 @@ import Flutter
 import UIKit
 import XCTest
 
+
 @testable import flutter_rotation_sensor
 
 // This demonstrates a simple unit test of the Swift portion of this plugin's implementation.
