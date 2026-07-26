43861c3d7ad378f73052d3ae4652ae1862eafc5c
6y <tlserver6y@gmail.com>
2026-07-26 20:51:53 +0800

    test: rewrite coordinate system test to remove mocking of NativeDeviceOrientation

--- test/coordinate_system_test.dart
+++ test/coordinate_system_test.dart
@@ -1,79 +1,26 @@
-import 'dart:async';
-
 import 'package:flutter_rotation_sensor/flutter_rotation_sensor.dart';
-import 'package:flutter_rotation_sensor/src/rotation_sensor_method_channel.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
 import 'package:native_device_orientation/native_device_orientation.dart';
-
-@GenerateNiceMocks([MockSpec<NativeDeviceOrientationCommunicator>()])
-import 'coordinate_system_test.mocks.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  final platform = RotationSensorMethodChannel();
-  final binaryMessenger =
-      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger;
-  const methodChannel = RotationSensorMethodChannel.methodChannel;
-  const orientationChannel = RotationSensorMethodChannel.eventChannel;
-  // ignore: close_sinks
-  late StreamController<void> oeStreamController;
-
-  setUp(() {
-    oeStreamController = StreamController<void>();
-    binaryMessenger
-      ..setMockMethodCallHandler(methodChannel, (methodCall) async => null)
-      ..setMockStreamHandler(
-        orientationChannel,
-        MockStreamHandler.inline(
-          onListen:
-              (args, sink) => oeStreamController.stream.listen(
-                (_) => sink.success([
-                  // Quaternion
-                  0.0, 0.0, 0.0, 1.0,
-                  // Accuracy
-                  -1.0,
-                  // Timestamp
-                  123456789,
-                ]),
-                onDone: () => sink.endOfStream(),
-              ),
-        ),
-      );
-  });
-
-  tearDown(() async {
-    await oeStreamController.sink.close();
-    binaryMessenger
-      ..setMockMethodCallHandler(methodChannel, null)
-      ..setMockStreamHandler(orientationChannel, null);
-  });
-
-  test(
-    'orientationStream emit OrientationEvent in device coordinate system',
-    () async {
-      RotationSensor.coordinateSystem = CoordinateSystem.device();
-      oeStreamController.sink.add(null);
-      final orientationEvent = await platform.orientationStream.first;
-      expect(orientationEvent.coordinateSystem, equals(Matrix3.identity()));
-    },
+  final sourceEvent = OrientationEvent(
+    quaternion: Quaternion(0, 0, 0, 1),
+    accuracy: -1,
+    timestamp: 123456789,
   );
 
+  test('DeviceCoordinateSystem remap OrientationEvent to itself', () async {
+    final deviceCoordinateSystem = DeviceCoordinateSystem();
+    final result = deviceCoordinateSystem.apply(sourceEvent);
+    expect(result.coordinateSystem, equals(Matrix3.identity()));
+  });
+
   test(
-    'orientationStream emit OrientationEvent in display coordinate system',
+    'DisplayCoordinateSystem remap OrientationEvent to display coordinate system',
     () async {
       final displayCoordinateSystem = DisplayCoordinateSystem();
-      RotationSensor.coordinateSystem = displayCoordinateSystem;
-      final ndoStreamController =
-          StreamController<NativeDeviceOrientation>.broadcast();
-      final mockCommunicator = MockNativeDeviceOrientationCommunicator();
-      when(
-        mockCommunicator.onOrientationChanged(),
-      ).thenAnswer((_) => ndoStreamController.stream);
-      displayCoordinateSystem.communicator = mockCommunicator;
-      expect(displayCoordinateSystem.communicator, equals(mockCommunicator));
 
       final orientations = [
         NativeDeviceOrientation.portraitUp,
@@ -82,68 +29,45 @@ void main() {
         NativeDeviceOrientation.landscapeLeft,
       ];
 
-      final orientationEventsFuture =
-          platform.orientationStream.take(orientations.length).toList();
-      for (final orientation in orientations) {
-        ndoStreamController.sink.add(orientation);
-        await Future.delayed(const Duration(microseconds: 1), () {});
-        oeStreamController.sink.add(null);
-        await Future.delayed(const Duration(microseconds: 1), () {});
-      }
-      final orientationEvents = await orientationEventsFuture;
-
       for (
         var t = 0, e = Matrix3.identity();
-        t < orientationEvents.length;
+        t < orientations.length;
         t++, e = e.multiply(Matrix3(0, -1, 0, 1, 0, 0, 0, 0, 1))
       ) {
-        final orientationEvent = orientationEvents[t];
+        displayCoordinateSystem.orientation = orientations[t];
+        final orientationEvent = displayCoordinateSystem.apply(sourceEvent);
         expect(
           orientationEvent.coordinateSystem,
           equals(e),
           reason: 'orientationEvents[$t]',
         );
       }
-
-      ndoStreamController.sink.add(NativeDeviceOrientation.portraitUp);
-      await ndoStreamController.close();
     },
   );
 
-  test('orientationStream emit error in display coordinate system', () async {
-    final displayCoordinateSystem = DisplayCoordinateSystem();
-    RotationSensor.coordinateSystem = displayCoordinateSystem;
-    final ndoStreamController =
-        StreamController<NativeDeviceOrientation>.broadcast();
-    final mockCommunicator = MockNativeDeviceOrientationCommunicator();
-    when(
-      mockCommunicator.onOrientationChanged(),
-    ).thenAnswer((_) => ndoStreamController.stream);
-    displayCoordinateSystem.communicator = mockCommunicator;
-    expect(displayCoordinateSystem.communicator, equals(mockCommunicator));
-
-    ndoStreamController.sink.add(NativeDeviceOrientation.unknown);
-    await Future.delayed(const Duration(microseconds: 1), () {});
-    oeStreamController.sink.add(null);
-    await Future.delayed(const Duration(microseconds: 1), () {});
-    await expectLater(() => platform.orientationStream.first, throwsStateError);
-
-    ndoStreamController.sink.add(NativeDeviceOrientation.portraitUp);
-    await ndoStreamController.close();
-  });
+  test(
+    'DisplayCoordinateSystem.apply emit error for unknown orientation',
+    () async {
+      final displayCoordinateSystem = DisplayCoordinateSystem()
+        ..orientation = .unknown;
+      expect(
+        () => displayCoordinateSystem.apply(sourceEvent),
+        throwsStateError,
+      );
+    },
+  );
 
   test(
-    'orientationStream emit OrientationEvent in transformed coordinate system',
+    'TransformedCoordinateSystem remap OrientationEvent to custom coordinate system',
     () async {
-      RotationSensor.coordinateSystem = CoordinateSystem.transformed(
+      final transformedCoordinateSystem = TransformedCoordinateSystem(
         Axis3.X,
         Axis3.Z,
-        CoordinateSystem.transformed(Axis3.X, Axis3.Y),
+        TransformedCoordinateSystem(Axis3.X, Axis3.Y, DeviceCoordinateSystem()),
       );
-      oeStreamController.sink.add(null);
-      final orientationEvent = await platform.orientationStream.first;
+      final result = transformedCoordinateSystem.apply(sourceEvent);
       expect(
-        orientationEvent.coordinateSystem,
+        result.coordinateSystem,
         equals(Matrix3(1, 0, 0, 0, 0, -1, 0, 1, 0)),
       );
     },
